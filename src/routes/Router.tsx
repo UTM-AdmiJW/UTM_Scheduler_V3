0e42c634d73400b8e3b7507096cf122efeb52147
AdmiJW <jamweg22@gmail.com>
2022-11-16 17:59:47 +0800

    Initial commit

---
+++ src/routes/Router.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
+
+
+export default function Router() {
+    return <HashRouter>
+        <Routes>
+            <Route path="/" element={ <div>Home</div> } />
+            <Route path="*" element={ <div>404</div>} />
+        </Routes>
+    </HashRouter>
+}
