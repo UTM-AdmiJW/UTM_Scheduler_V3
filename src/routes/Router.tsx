669f7d56dc300539a1629bbbe2df230d90cab136
WindMotion <107300466+yk-zzz@users.noreply.github.com>
2022-12-02 00:04:23 +0800

    CYK: Branch Test

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -6,7 +6,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 export default function Router() {
     return <HashRouter>
         <Routes>
-            <Route path="/" element={ <div>Bello</div> } />
+            <Route path="/" element={ <div>Branchhhh</div> } />
             <Route path="*" element={ <div>404</div>} />
         </Routes>
     </HashRouter>
