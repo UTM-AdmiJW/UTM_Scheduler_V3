558c702a55f9721e42b238e2565e14e2eb930951
Ethanlyt <ethanleong86@gmail.com>
2022-12-02 00:12:15 +0800

    Et: What

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -6,7 +6,9 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 export default function Router() {
     return <HashRouter>
         <Routes>
-            <Route path="/" element={ <div>Testing In Ethan Branch</div> } />
+            <Route path="/" element={ <div>Testing In Ethan Branch 
+                <span>WHATTTTTTTTTTTTTTTTTTTTTTTTTTTTT??</span>
+                 </div> } />
             <Route path="*" element={ <div>404</div>} />
         </Routes>
     </HashRouter>
