b77b1429a0d842aae9498426d74b0bc007009a35
AdmiJW <jamweg22@gmail.com>
2023-01-06 03:05:16 +0800

    JW: Done Registered Courses Login

--- src/views/registeredCourses/login/RegisteredCoursesLoginView.tsx
+++ src/views/registeredCourses/login/RegisteredCoursesLoginView.tsx
@@ -1,21 +1,49 @@
-import { Button, DialogActions, DialogContent } from '@mui/material';
+import { Button, DialogActions, DialogContent, Typography } from '@mui/material';
+import LoginDialog from '../../login/LoginDialog';
 
+import { useEffect } from 'react';
 import { useDialog } from '../../../hooks/useDialog';
 import { useStudentRedux } from '../../../hooks/redux/useStudentRedux';
+import { useRegisteredCoursesContext } from '../../../hooks/context/useRegisteredCoursesContext';
 
 import { BsPersonXFill } from 'react-icons/bs';
+import { RegisteredCoursesProgress } from '../../../enums/RegisteredCoursesProgress';
 
 
 
 export default function RegisteredCoursesLoginView() {
 
-    const { closeDialog } = useDialog();
+    const { openDialog, closeDialog } = useDialog();
     const { studentState: { isLoggedIn } } = useStudentRedux();
+    const { setRegisteredCoursesState } = useRegisteredCoursesContext();
+
+
+    const handleLoginButton = ()=> {
+        openDialog(<LoginDialog />);
+    }
+
+    useEffect(()=> {
+        if (!isLoggedIn) return;
+
+        setRegisteredCoursesState(prev => {
+            return { ...prev, progress: RegisteredCoursesProgress.SELECT_REGISTERED_SUBJECT };
+        });
+    }, [isLoggedIn]);
 
 
     return <>
-        <DialogContent className='pt-2'>
+        <DialogContent className='pt-4 flex flex-col items-center'>
 
+            <BsPersonXFill className='text-5xl text-gray-400' />
+
+            <Typography className='text-center text-gray-400 mt-2 mb-4 font-extralight'>
+                You must be logged in in order to view your registered <br/>
+                courses using your matric number.
+            </Typography>
+
+            <Button variant='contained' color='primary' onClick={handleLoginButton}>
+                Login
+            </Button>
         </DialogContent>
 
         <DialogActions className='mt-2'>
